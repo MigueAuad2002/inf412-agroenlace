00778c7f34e79843b26f4684e3d1028cfe762daf
Miguel Auad <migueauad@icloud.com>
2026-03-15 14:22:36 -0400

    estructurando frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>🚀 Frontend de Agroenlace Inicializado</h1>
+      <p>Esperando conexión con el Backend...</p>
+    </div>
+  )
+}
+
+export default App
